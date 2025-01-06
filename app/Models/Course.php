8a1259ad22bbc7bea7c74c2c20d846c74d44109c
samurai <samuraiprogramer@gmail.com>
2025-01-06 20:07:36 +0500

    models

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function lessons(){
+        return $this->hasMany(Lesson::class);
+    }
 }
--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -4,9 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use \Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Lesson extends Model
 {
-    /** @use HasFactory<\Database\Factories\LessonFactory> */
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function students()
+    {
+        return $this->belongsTo(Student::class, 'registrations');
+    }
+
+    public function course() {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function teacher() {
+        return $this->belongsTo(Teacher::class);
+    }
+
+    public function season() {
+        return $this->belongsTo(Season::class);
+    }
+
+    public function location() {
+        return $this->belongsTo(Location::class);
+    }
+
 }
